1fe77b299790696b2e6f57a5303fb9b93313a37c
VicZambom <140662019+VicZambom@users.noreply.github.com>
2025-12-06 17:33:35 +0000

    chore: configuração do provedor do TanStack Query no App.tsx

--- hello-rn/App.tsx
+++ hello-rn/App.tsx
@@ -1,19 +1,27 @@
 // App.tsx
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { SocketProvider } from './src/context/SocketContext';
 import { SyncProvider } from './src/context/SyncContext';
 
+// Criação do cliente do React Query
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <AuthProvider>
-      <SocketProvider>
-        <SyncProvider> 
-          <StatusBar style="light" />
-          <AppNavigator />
-        </SyncProvider> 
-      </SocketProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <SocketProvider>
+          <SyncProvider> 
+            <StatusBar style="light" />
+            <AppNavigator />
+          </SyncProvider> 
+        </SocketProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
